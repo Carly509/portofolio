49b764505a74411de848f58b66f84b2d5f927814
carly509 <tesnorcarly@gmail.com>
2024-01-14 00:22:26 -0500

    hotfix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8a9d104652766889"; }
+/******/ 		__webpack_require__.h = function() { return "91a569d351e7be3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -33,16 +33,19 @@ const Articles = () => {
     }
   })} */}
   <h1>Understanding and fixing N+1 query in Ruby on Rails</h1>
-  <p>Regarding backend performance, there is a performance issue that everybody has heard about at least once: N+1 query. The N+1 query problem happens when your code executes N additional query statements to fetch the same data that could have been retrieved when executing the primary query.</p>
-  <p>When it comes to understanding and fixing N+1 queries in the context of Ruby on Rails, it refers to the process of identifying and resolving a common performance issue in database queries. An N+1 query occurs when a database query is made within a loop, resulting in N+1 individual queries being executed. This can lead to inefficient database access and slow down the application's performance.</p>
-  <p>In the context of Ruby on Rails, this issue can be addressed by using techniques such as eager loading, which allows related data to be loaded in advance, reducing the number of database queries. Additionally, caching and optimizing database indexes can also help improve query performance.</p>
-  <p>To fix N+1 queries in Ruby on Rails, developers can use tools like ActiveRecord's includes method to preload associations and minimize the number of database queries. They can also use tools like Bullet gem to detect and address N+1 query issues.</p>
+  <br/>
+  <p>Regarding backend performance, there is a performance issue that everybody has heard about at least once: N+1 query. </p>
+  <p> <strong>An N+1 query problem happens when your code executes N additional query statements to fetch the same data that could have been retrieved when executing the primary query.</strong> When it comes to understanding and fixing N+1 queries in the context of Ruby on Rails, it refers to the process of identifying and resolving a common performance issue in database queries</p>
+  <p>Let's start with an analogy first</p>
+  <p>Imagine you're preparing a meal and forget the buy all the ingredients. Now you have to go back to the groceries store to get them and everytime you forget one you have to make repeated trips ther (multiple feching). this is similar to the N+1 query problem in programming. When using an ORM (Object-Relational Mapping) framework, your code may inadvertently cause the ORM to execute N additional queries after the initial query, resulting in inefficient database access.</p>
+  <p>This issue can be addressed by using techniques such as eager loading, which allows related data to be loaded in advance, reducing the number of database queries. Additionally, caching and optimizing database indexes can also help improve query performance. Tools like ActiveRecord's includes method can preload associations and minimize the number of database queries. you can also use tools like Bullet gem to detect and address N+1 query issues.</p>
   <p>Let's say we have a blog application with two models: Post and Comment. Each post has multiple comments associated with it. Now, suppose we want to display a list of posts along with the number of comments for each post.</p>
-  <p>Example of N+1 Query (Not Good):</p>
-  <img src="img/img1.png" alt="bad implementation" />
+  <h5>Examples of N+1 Query </h5>
+  <p>Bad Implementation:</p>
+  <img src="img/img1.png" alt="bad implementation"  style={{ width: '500px', height: 'auto' }}/>
   <p>In this example, for each post, a separate query is made to fetch the comments associated with that post. This leads to N+1 queries, where N is the number of posts. As the number of posts grows, the number of database queries also grows, leading to performance issues.</p>
-  <p>Good Implementation:</p>
-  <img src="img/img2.png" alt="bad implementation" />
+  <p>Correct Implementation:</p>
+  <img src="img/img2.png" alt="bad implementation" style={{ width: '500px', height: 'auto' }}/>
   <p>In the good implementation, we use the includes method to eager load the comments associated with each post. This results in a single query to fetch all the posts and their associated comments, improving performance by reducing the number of database queries.</p>
   <p>By using the includes method, we can optimize the database access and avoid N+1 query issues in our Ruby on Rails application.</p>
 </TabItems>
